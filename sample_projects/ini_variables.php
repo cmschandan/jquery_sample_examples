7f4ae9aa37feeaec928bddb6b35347fb6703af88
cmschandan <chandan.cotocus@gmail.com>
2020-03-24 13:24:24 +0530

    adding javascript examples

---
+++ sample_projects/ini_variables.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Initializing Variable</title>
+</head>
+<body>
+	<script>
+
+		// Varibale define and initiliazation
+
+		// var price = 101;
+		// var price;
+		// document.write("Price :" +price);
+		// //second statement
+		// var product = "Apple";
+		// document.write("<br>Product Name: " + product);
+
+		// Operator in javascript
+		// var a = 10;
+		// var b = 20;
+		// var c = a+b;
+		// document.write("<br> Addition of a and b : "+c);
+
+		// var a = prompt("<br> Enter Value : ");
+		// document.write(a);
+
+		// if else condition
+		// var a = prompt("Enter Roll No: ");
+		// if(a == 20)
+		// document.write("Name : Chandan");
+		// else
+		// document.write("Hello buddy you are wrong");
+
+		//else if condition
+
+		var result = 75;
+		if(result <=30)
+			document.write("Fail");
+		else if(result <=40)
+			document.write("Pass");
+		else if(result <=60)
+			document.write("Good");
+		else 
+			document.write("Very Good");
+
+	</script>
+
+	<!-- <?php
+
+	$x =25;
+	$x = 30;
+	echo "$x";
+
+	$x = 25;
+
+	?> -->
+</body>
+</html>
